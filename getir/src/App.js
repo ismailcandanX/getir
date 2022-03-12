35a1fb009c705f7e783e76e67283d59663579951
ismailcandanX <ismailcandan44@gmail.com>
2022-03-12 22:41:12 +0300

    install tailwind and react

---
+++ getir/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-sky-700 text-center text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
